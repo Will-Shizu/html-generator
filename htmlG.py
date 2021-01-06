2dfc9dadc885a60b3031be021cc83e36fb774f44
Willyan (Shizu) <willshizu@gmail.com>
2021-01-06 13:59:37 -0300

    Work in progress

--- htmlG.py
+++ htmlG.py
@@ -1,6 +1,7 @@
 import os
 
 # METHODS
+# Text decoration
 def txtDec(txt, stl='none',cl='white'):
 	#font style
 	if stl == 'bold':
@@ -30,8 +31,10 @@ def txtDec(txt, stl='none',cl='white'):
 
 	return f"\033[{s};{c}m{txt}\33[m"
 
+# Menu?
 def menu(ops):
-	print(f"{10*' '}{txtDec('- MENU -', 'bold', 'yellow')}")
+	msgMenu = txtDec('- MENU -', 'bold', 'yellow')
+	print(f"{10*' '}{msgMenu}")
 	print()
 	for option in ops:
 		print(option)
@@ -47,9 +50,8 @@ def menu(ops):
 		except:
 			print("Invalid option!")
 
+# Create a new HTML file
 def initHtml(name, dire):
-	if name[-5:] != ".html":
-		name += ".html"
 	head = ["<!DOCTYPE html>"]
 	head.append("<html>")
 	head.append("<head>")
@@ -61,6 +63,95 @@ def initHtml(name, dire):
 	doc.writelines(head)
 	doc.close()
 
+# Get the position of a tag with the id
+def getPos(arc, slc):
+	doc = open(arc, 'r')
+	docContent = doc.readlines()
+	endHead = False
+	i = 0
+	for pos,line in enumerate(docContent):
+		if docContent[pos-1] == "</head>\n":
+			endHead = True
+		if endHead and "<" in line and ">" in line:
+			indent = line.count('	')
+			line.strip()
+			if "/" in line and line[line.index(">")-1] == "/":
+				if indent == 0:
+					i = int(i+1)
+				else:
+					i = round(i+0.1, 1)
+			elif "/" not in line:
+				if indent == 0:
+					i = int(i+1)
+				else:
+					i = round(i+0.1, 1)
+		if i == slc:
+			return pos
+	return False
+
+# Add tags for a HTML document
+def addHtml(tag, arc, slc=0):
+	self_close = ("area","base","br","col","command","embed","hr","img","input",
+		"keygen","link","menuitem","meta","param","source","track","wbr")
+
+	if slc == 0:
+		doc = open(arc, 'a')
+		if tag in self_close:
+			doc.write(f"<{tag}/>\n")
+		else:
+			doc.write(f"<{tag}>\n")
+			doc.write(f"</{tag}>\n")
+		doc.close()
+	else:
+		doc = open(arc, 'r')
+		docContent = doc.readlines()
+		pos = getPos(arc, slc)
+		indent = docContent[pos].count('	') + 1
+		n = pos+1
+		for line in docContent[n:]:
+			if line.count('	') <= docContent[pos].count('	'):
+				if tag in self_close:
+					docContent.insert(n,f"{indent*'	'}<{tag}/>\n")
+				else:
+					docContent.insert(n, f"{indent*'	'}<{tag}>\n")
+					docContent.insert(n+1, f"{indent*'	'}</{tag}>\n")
+				break
+			else:
+				n+=1
+		doc = open(arc, 'w')
+		doc.writelines(docContent)
+		doc.close()
+
+# Show the tags of a HTML
+def listHtml(arc):
+	doc = open(arc, 'r')
+	docContent = doc.readlines()
+	endHead = False
+	i = 0
+	for pos,line in enumerate(docContent):
+		if docContent[pos-1] == "</head>\n":
+			endHead = True
+		if endHead and "<" in line and ">" in line:
+			show = False
+			indent = line.count('	')
+			line.strip()
+			if "/" in line and line[line.index(">")-1] == "/":
+				showLine = line[line.index("<")+1:line.index("/")]
+				if indent == 0:
+					i = int(i+1)
+				else:
+					i = round(i+0.1, 1)
+				show = True
+			elif "/" not in line:
+				showLine = line[line.index("<")+1:line.index(">")]
+				if indent == 0:
+					i = int(i+1)
+				else:
+					i = round(i+0.1, 1)
+				show = True
+			if show:
+				print(f"{indent*'   '}{i}-{showLine}")
+
 # MAIN CODE
 # Seting requirements
 fileDir = os.path.dirname(os.path.realpath(__file__))
@@ -70,10 +161,12 @@ if "html" not in dirCont:
 
 # Menu 
 op = menu(["1-Create HTML", "2-Modify HTML", "3-Exit"]) # <= Menu options
-print(30*'-')
 
 if op == 1:
+	print(30*'-')
 	docName = str(input("Document name: ")).strip()
+	if docName[-5:] != ".html":
+		docName += ".html"
 	print()
 	print("Leave in blank to save in the default diretory:")
 	print(os.path.join(fileDir, "html"))
@@ -81,5 +174,29 @@ if op == 1:
 	docDir = str(input("Document diretory: ")).strip()
 	if docDir == '':
 		docDir = os.path.join(fileDir, "html")
-
 	initHtml(docName, docDir)
+	arc = os.path.join(docDir, docName)
+	# Comands
+	slc = 0
+	while(True):
+		cmd = str(input("\nYour comand: ")).split(',')
+		for c in cmd:
+			c = c.strip()
+			if c ==  "--exit":
+				break
+			if "slc" in c:
+				slc = float(c.split(' ')[1])
+				exist = getPos(arc, slc)
+				if not exist:
+					print("This item not exist!")
+					slc = 0
+			else:
+				addHtml(c, arc, slc)
+				print(f"\n-> {docName}")
+				listHtml(arc)
+		if c == "--exit":
+			break
+
+# rmHtml()
+# slcHtml()
+# editHtml()
